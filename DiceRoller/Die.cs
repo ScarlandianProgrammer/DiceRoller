3100d92de8069a71b400d6fe49d1d365c0dd232d
Elias McKee <eqbookfan@gmail.com>
2023-07-17 14:44:05 -0700

    Initial Commit

---
+++ DiceRoller/Die.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    /// <summary>
+    /// This class makes an object that represents a single, six-sided die.
+    /// </summary>
+    public class Die
+    {
+        /// <summary>
+        /// The value of the die's current top side.
+        /// </summary>
+        public byte FaceValue { get; set; }
+
+        /// <summary>
+        /// True it the die is held.
+        /// </summary>
+        public bool IsHeld { get; set; }
+
+        /// <summary>
+        /// Rolls the die and sets the <see cref="FaceValue"> to the random number.
+        /// </summary>
+        /// <returns>Returns the new <see cref="FaceValue"/> </returns>
+        public byte Roll()
+        {
+            throw new NotImplementedException();
+            /*if (!IsHeld)
+            {
+                FaceValue = ;
+            }*/
+        }
+    }
+}
